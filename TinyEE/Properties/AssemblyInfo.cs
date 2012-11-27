344a602838e457322583a4819492162e8ca516d5
hvu <hvu.0x7c3@gmail.com>
2012-11-27 16:59:55 +0700

    Update assembly info

--- TinyEE/Properties/AssemblyInfo.cs
+++ TinyEE/Properties/AssemblyInfo.cs
@@ -1,27 +1,16 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("TinyEE")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Tiny Expression Evaluator")]
+[assembly: AssemblyDescription("A simple expression language running on .NET's Dynamic Language Runtime")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("hvu")]
 [assembly: AssemblyProduct("TinyEE")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5347cbff-0a00-46d2-a79c-ae1951f698fd")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
